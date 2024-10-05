9c1dfe5fd609b7e8ccf586aa10434371bb1e5749
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-05 22:45:13 +0600

    use input phone in form

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -4,7 +4,8 @@ const SimpleForm = () => {
         e.preventDefault();
         console.log(e.target.name.value);
         console.log(e.target.email.value);
-        console.log('form submitted')
+        console.log(e.target.phone.value)
+;        console.log('form submitted')
     }
     return (
         <div>
@@ -12,6 +13,8 @@ const SimpleForm = () => {
                 <input type="text" name="name" />
                 <br />
                 <input type="email" name="email" id="" />
+                <br />
+                <input type="text" name="phone" />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
