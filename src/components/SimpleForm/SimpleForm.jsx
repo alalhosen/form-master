8030fe0b037c4624872508c3d4078108aa19faff
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-05 22:36:49 +0600

    add input & use email

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,17 @@
-
 const SimpleForm = () => {
+
+    const handleSubmit = e=>{
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log('form submitted')
+    }
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" id="" />
                 <br />
                 <input type="submit" value="Submit" />
             </form>
