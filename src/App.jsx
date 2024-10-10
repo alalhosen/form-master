e382d4c3503984308a932ce433fef33ff66d5a37
Alal Hosen <alalhosen1992620120@gmail.com>
2024-10-10 22:06:12 +0600

    create ReusableForm.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import StatefulForm from './components/StatefulForn/StatefulForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
@@ -12,7 +13,8 @@ function App() {
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm></ReusableForm>
     </>
   )
 }
